1de63d7c09beb08fbdaddf56b237f0498504d8c7
fwang29 <fwang29@illinois.edu>
2015-07-14 20:53:49 -0500

    modified a lot but still buggy

--- app/static/js/gantt_chart.js
+++ app/static/js/gantt_chart.js
@@ -1,22 +1,20 @@
 /*
-	1. task obj: name, start_date, end_date, progress
+
 	2. server side
 	3. calendar table
-	4. pop-out
-	5. left-panel displaying name, startDate, endDate
-	6. right-panel displaying bars mapped by week number
-	7. combine two tables next to each other
-	8. use pure text to represent progress bar (not-responsive..)
+	4. pop-out (modal?)
+	
 
-	problems:
-	1. tables next to each other
-	2. progress bar cannot show up
-	3. bar width 
-	4. dynamic scaling weeks
+	7/14 Tasks
+	2. data binding (angularJS?
+	3. delete (buggily finished), modify
 */
 
-/* array to hold tasks objs */
+/* global array to hold tasks objs */
 var tasks = new Array();
+/* var to hold current number of weeks */
+var weeks = 7; 
+
 
 /* task object ctor*/
 function Task (number, taskName, startDate, endDate, progress) {
@@ -51,41 +49,52 @@ function diffDays(date1, date2)
 }
 
 /***** generate the progress bar for the gantt chart using bootstrap built-in feature *****/
-function generateBar (percent) {
+function generateBar (task) {
 	//calculate the length of the completed bar 
-	var progress = percent;
-	var i = tasks.length - 1;
-	var totalLength = diffDays(tasks[i].startDate, tasks[i].endDate);
-	var completed = Math.floor(tasks[i].progress * totalLength);
-	var unfinished = totalLength - completed;
+	var days = diffDays(task.startDate, task.endDate);
+	var barLength = Math.floor(days/(weeks*7)*100);
+	barLength += '%';
+
+	//current task progress in percent
+	var progressLength = task.progress + '%';
 	
-	/* create a tr with div that looks like below:
-		<tr><td>
+	/* create a td with div that looks like below:
+		<td>
 		<div class="progress" width=13%>
 		<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
     		60%
   		</div>
   		</div>
-  		</tr></td>
+  		</td>
   	*/
-	var $row = '<tr><td><div class="progress" style="width: ' + totalLength/365 + ';"><div class="progress-bar" role="progressbar" aria-valuenow="'
-					+ progress 
+	var $bar = '<div class="progress" style="display: inline-block; width: ' + barLength + ';"><div class="progress-bar" role="progressbar" aria-valuenow="'
+					+ task.progress 
 					+ ' aria-valuemin="0" aria-valuemax="100" style="width: '
-					+ progress +';">' + progress + '</div></div></tr></td>';         
+					+ progressLength +';">' + progressLength + '</div></div>';         
 	
+	//calculate position of the bar
+	var weekDiv = scaleWeeks();    //get the div width of each week number 
+	var divsBeforeBar = diffDays(document.getElementById("startWeek").value, task.startDate) / 7;     //cannot be < 0 
 
-	$('.chart').append($row);
+	var $completeBar = '';
+
+	for (var i = 1; i <= divsBeforeBar; i++) {
+		$completeBar += '<div style="display: inline-block; width: ' + weekDiv + '">' + " " + '</div>';
+	};
+	$completeBar += $bar;
+
+	return $completeBar;
 }
 
 /* generate gantt chart table row with newly added tasks array elem */
-function addTableRow() {
-	var i = tasks.length - 1;
-	var $row = '<tr><td>'+ i
-		   						+'</td><td>'+tasks[i].taskName
-		   						+'</td><td>'+tasks[i].startDate
-		   						+'</td><td>'+tasks[i].endDate
-		   						+'</td></tr>';
-			
+function addTableRow(task) {
+	var $row = '<tr><td>'+ task.number
+		   						+'</td><td>'+task.taskName
+		   						+'</td><td>'+task.startDate
+		   						+'</td><td>'+task.endDate
+		   						+'</td><td><button class="btn btn-default" onclick="deleteTask(this)">delete</button></td>'
+								+ '<td>' + generateBar(task) + '</td>'
+								+ '</tr>';
 	$('.table').append($row);
 }
 
@@ -99,18 +108,47 @@ function addTask() {
     //document.getElementById("result").innerHTML = [name, startDate, endDate, progress];
     var task = new Task(tasks.length, name, startDate, endDate, progress);
     tasks.push(task);
-    addTableRow();
-    generateBar(progress);
+    addTableRow(task);
 }
 
 /***** delete task *****/
-function deleteTask() {
-
+function deleteTask(r) {
+	var rowNum = r.parentNode.parentNode.rowIndex;         //get current row number
+	document.getElementById("gantt_chart_table").deleteRow(rowNum);    //delete current row of chart
+	tasks.splice(rowNum, 1);   //buggy
 }
 
 
-/***** weeks scaling *****/
+/***** weeks scaling once the reset button is clicked *****/
 function scaleWeeks() {
+	var start = document.getElementById("startWeek").value;
+	var end = document.getElementById("endWeek").value;
+	var days = diffDays(start, end);
+	weeks = Math.floor(days/7);
+	var percent = Math.floor(1/weeks * 100);
+	
+	if (percent == 1) {
+		alert("week scale is too large");
+		return;
+	}
+	
+	percent += '%';
+
+	var $row = '';
+	for (var i = 1; i <= weeks; i++) {
+		//create div like: <div style="width: 20%"> i <div>
+		$row += '<div style="display: inline-block; width: ' + percent + ';">' + i + '</div>';
+	}
+	document.getElementById("weeks_number").innerHTML = $row;	
+	return percent;
+}
+
+function changeID(r) {
 	
 }
 
+
+
+$(document).ready(function(){
+    scaleWeeks();
+});
